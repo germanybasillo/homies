9e6e55e89a5a7867e0b3ebb8973df268db48f39d
Titus <gtnagar@gmail.com>
2024-07-09 21:33:12 +0800

    Update Bedassign.php
    define relationships

--- app/Models/Bedassign.php
+++ app/Models/Bedassign.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Bedassign extends Model
 {
@@ -16,4 +17,24 @@ class Bedassign extends Model
         'start_date',
         'due_date'
     ];
+
+    /**
+     * Get the Roon 
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class,'room_id','id');
+    }
+    
+    /**
+     * Get the Roon 
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function bed(): BelongsTo
+    {
+        return $this->belongsTo(Bed::class,'bed_id','id');
+    }
 }
