4135e5d0d180b5bc78c1f3840c1532532485145f
germany <germanybasillo@gmail.com>
2024-07-12 00:01:43 +0800

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,4 +12,24 @@
         </a>
     </div>
 
+    @if (session('tenant'))
+    <div style="color: green;text-align:center;background-color:white;border-color:white;border-radius:5px 5px;font-size:20px;">
+        Logout from tenant account successfully!
+    </div>
+    @endif
+
+    @if (session('rental_owner'))
+    <div style="color: blue;text-align:center;background-color:white;border-color:white;border-radius:5px 5px;font-size:20px;">
+        Logout from rental_owner account successfully!
+    </div>
+    @endif
+
+    @if (session('admin'))
+    <div style="color: red;text-align:center;background-color:white;border-color:white;border-radius:5px 5px;font-size:20px;">
+        Logout from admin account successfully!
+    </div>
+    @endif
+    
+ 
+
 </x-guest-layout>
